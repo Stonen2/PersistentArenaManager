c8820cfb9c822b2abdf5897ef2919a990b2adf78
Nick Stone <35820605+Stonen2@users.noreply.github.com>
2019-07-05 11:34:34 -0400

    Add files via upload

--- Summer2019-master/Summer2019-master/Experiment/Allocator/tests.cpp
+++ Summer2019-master/Summer2019-master/Experiment/Allocator/tests.cpp
@@ -1,5 +1,8 @@
 //#include "TA.h"
-#include "mapalloc.h"
+//#include "mapalloc.h"
+#include "srmap.h"
+#include "cll.h"
+#include "srmm.h"
 #include <iostream>
 using namespace std; 
 
@@ -13,18 +16,80 @@ int main() {
 	int c = 4; 
 	int d = 10; 
 
-	size_t j = sizeof(a);
-	mapalloc m(j);
+	srmm Mapping;
 
-	int * f = static_cast <int*>(m.malloc(sizeof(b)));
-	int * g = static_cast <int*>(m.malloc(sizeof(c)));
-	int * h = static_cast <int*>(m.malloc(sizeof(d)));
+	cll l; 
+	cout << "The value is" << endl; 
+	//cout << tfds << endl;
+	l.insert(4);
+	srmap Testing;
+	void* tfds = Testing.malloc(sizeof(a));
+	void* tt = Testing.malloc(sizeof(b));
+	l.print(); 
+	cout << endl; 
+	cout << "------------" << endl;
+	size_t sa = sizeof(a);
+	size_t sb = sizeof(b);
+	Testing.insert(tfds,sa);
+	Testing.insert(tt,sb);
+	cout << endl; 
 
+	cout << endl; 
 
 	
-	cout << &f << " F" << endl; 
-	cout << &g << " G" << endl; 
-	cout << &h << " D" << endl;
+	//cout << Testing; 
+
+	cout << "--------";
+	cout << endl;
+	Testing.print();
+	cout << endl; 
+
+	//Testing.print();
+
+	//Testing.insert(5);
+
+
+	size_t j = sizeof(a);
+	//mapalloc m(j);
+
+	//int * f = static_cast <int*>(m.malloc(sizeof(b)));
+	//int * g = static_cast <int*>(m.malloc(sizeof(c)));
+	//int * h = static_cast <int*>(m.malloc(sizeof(d)));
+
+	cout << endl;
+	cout << "------------------------------------------------" << endl; 
+	
+	int lma = 5; 
+	int lmb = 4; 
+
+	void* lmaa = Mapping.malloc(sizeof(lma));
+	void* lmbb = Mapping.malloc(sizeof(lmb));
+
+	size_t lmaaa = sizeof(lma);
+	size_t lmbbb = sizeof(lmb);
+	Mapping.insert(lmaa,lmaaa);
+	Mapping.insert(lmbb,lmbbb);
+	cout << endl;
+	cout << "Its Starting" << endl; 
+	Mapping.print();
+	cout << endl; 
+
+	
+	/*
+	Test Mapping 
+	
+	*/
+
+
+
+
+	cout << endl; 
+	cout << "Mapping Complete" << endl; 
+	cout << "-------------------------------------------------" << endl; 
+	
+	//cout << &f << " F" << endl; 
+	//cout << &g << " G" << endl; 
+	//cout << &h << " D" << endl;
 
 	//allocator a(f); 
 
