c3d0fa325470b0ec6f9f2c522e53136b2d303aaa
Nick Stone <35820605+Stonen2@users.noreply.github.com>
2019-07-08 11:32:02 -0400

    Add files via upload
    Adding Comments Now

--- Summer2019-master/Summer2019-master/Experiment/Self Referential/tests.cpp
+++ Summer2019-master/Summer2019-master/Experiment/Self Referential/tests.cpp
@@ -11,42 +11,65 @@ using namespace std;
 
 
 int main() {
+
+	//ALlocate some simple variables to store in the Linked List
 	int a = 5;
 	int b = 3; 
 	int c = 4; 
 	int d = 10; 
 
+	//Instantiate the MMAP allocator
 	srmm Mapping;
 
+
+	/*
+	Basic Linked List Testing
+	*/
 	cll l; 
 	cout << "The value is" << endl; 
-	//cout << tfds << endl;
 	l.insert(4);
+	l.print();
+	cout << endl;
+
+	//Start testing the Simple Allocator
+
 	srmap Testing;
 	void* tfds = Testing.malloc(sizeof(a));
-	void* tt = Testing.malloc(sizeof(b));
-	
-	
-	l.print(); 
-	cout << endl; 
+	void* tt = Testing.malloc(sizeof(b));  
 	cout << "------------" << endl;
+
+
 	size_t sa = sizeof(a);
 	size_t sb = sizeof(b);
 	cout << tt << endl << "void * ";
+	//Insert the first couple of values
 	Testing.insert(tfds,sa);
 	Testing.insert(tt,sb);
+	
+	
 	cout << "insert works" << endl;
+
+	//First iteration of the free function... Optimized in MMAP 
 	int dd = Testing.free(tt);
+	
+	
 	cout << endl; 
 	cout << dd; 
 	cout << endl; 
 	cout << "Free works";
+	
+	//Make Sure we are NOT allocating a new block
 	void* thisis = Testing.malloc(sizeof(c));
 
 
 	cout << thisis << endl << "HERE IT IS";
+
+
+
 	size_t sc = sizeof(c);
 	
+
+	//No reason to insert this into the list since the Node does not leave but testing Linked List
 	Testing.insert(thisis, sc);
 	cout << "********************************************************************************************************";
 
@@ -63,9 +86,7 @@ int main() {
 	Testing.print();
 	cout << endl; 
 
-	//Testing.print();
-
-	//Testing.insert(5);
+	//END SIMPLE ALLOCATOR
 
 
 	size_t j = sizeof(a);
@@ -75,80 +96,45 @@ int main() {
 	//int * g = static_cast <int*>(m.malloc(sizeof(c)));
 	//int * h = static_cast <int*>(m.malloc(sizeof(d)));
 
+
+	//Begin MMAP Allocator
 	cout << endl;
 	cout << "------------------------------------------------" << endl; 
 	
 	int lma = 5; 
 	int lmb = 4; 
+	int lmc = 6; 
 
+
+	//Test the Malloc
 	void* lmaa = Mapping.malloc(sizeof(lma));
 	void* lmbb = Mapping.malloc(sizeof(lmb));
 
 	size_t lmaaa = sizeof(lma);
 	size_t lmbbb = sizeof(lmb);
+	size_t lmccc = sizeof(lmc);
+	//Test Inserting into the Linked List Data Structure
 	Mapping.insert(lmaa,lmaaa);
 	Mapping.insert(lmbb,lmbbb);
 	cout << endl; 
+	//Test Freeing one of the blocks of memory 
 	Mapping.free(lmaa);
-	
+	void* lmcc = Mapping.malloc(sizeof(lmc));
+	Mapping.insert(lmcc, lmccc);
+
+
+	//Print the Map of the linked List
+
 	cout << endl;
 	cout << "Its Starting" << endl; 
 	cout << "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&" << endl;
 	Mapping.print();
 	cout << endl; 
-
-	
-	/*
-	Test Mapping 
-	
-	*/
-
-
-
-
 	cout << endl; 
 	cout << "Mapping Complete" << endl; 
 	cout << "-------------------------------------------------" << endl; 
-	
-	//cout << &f << " F" << endl; 
-	//cout << &g << " G" << endl; 
-	//cout << &h << " D" << endl;
+	cout << "PROGRAM TERMINATED";
 
-	//allocator a(f); 
-
-
-
-	
-
-	//TA test(sizeof(a)); 
-	//void* t = test.start; 
-
-	//cout << &t + 512 << "TESTING" << endl; 
-
-	//cout << &t << endl;
-	 
-	//allocator a; 
-
-	//int* p = static_cast <int*>(test.malloc(a));
-	
-	
-	//cout << "HAHA" << endl; 
-	//int s = t; 
-	//int ff = p; 
-
-	//cout << t - p << endl;
-	//cout << &p;
-	cout << "OBJEXT HAS BEEN BUILT";
-
-
-
-	//cout << test.start + test.next;
-	//cout << p; 
-	cout << endl; 
-	//cout << test.HeapSize;
-	cout << endl; 
-
-	//cout << test.next;
 
 
 }
