82c3a61ade9c3d1067955ae505dd391ee4b488df
Nick Stone <35820605+Stonen2@users.noreply.github.com>
2019-07-09 13:47:48 -0400

    Add files via upload
    Recovering a Bit map of all of the Memory Locations

--- Summer2019-master/Summer2019-master/Experiment/bitmaps/bitmaps/Runit.cpp
+++ Summer2019-master/Summer2019-master/Experiment/bitmaps/bitmaps/Runit.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "CrashRecoveryBitMap.h"
-
+#include "srmap.h"
 
 using namespace std; 
 
@@ -8,21 +8,73 @@ using namespace std;
 
 int main() {
 
+	int a = 5; 
+	int b = 6;  
+	int d = 8; 
+	int gggg = 5; 
+	srmap Testing;
+	void* tfds = Testing.malloc(sizeof(a));
+	void* tts = Testing.malloc(sizeof(b));
+	void* tst = Testing.malloc(sizeof(d));
+	void* tgg = Testing.malloc(sizeof(gggg));
+	cout << "------------" << endl;
+
+	size_t sd = sizeof(d);
+	size_t sa = sizeof(a);
+	size_t sb = sizeof(b);
+	size_t sggg = sizeof(gggg);
+	cout << tts << endl << "void * ";
+	//Insert the first couple of values
+	Testing.insert(tfds, sa);
+	Testing.insert(tts, sb);
+	Testing.insert(tst, sd);
+	Testing.insert(tgg, sggg);
+	cout << "Insert finished";
+
+
+	cout << "********************************************************" << endl; 
+	Testing.print();
+	cout << endl; 
 
 	size_t t = 8;
-	void* tt = malloc(sizeof(t)); 
+	void* tt = Testing.malloc(t);
 	void* ttt = malloc(t);
+	//void* tt = malloc(sizeof(t)); 
+	//void* ttt = malloc(t);
 	CrashRecoveryBitMap m(t,tt); 
 	cout << endl; 
 
-	cout << tt; 
+	//cout << tt; 
+	cout << endl; 
+	//cout << ttt; 
+	cout << "************************************88" << endl; 
+	
+	Testing.print();
+	cout << endl; 
+	cout << tt;
 	cout << endl; 
 	cout << ttt; 
+	cout << endl;
+	cout << "******************************************8";
 	
+	
+	
+	//cout << "Program Terminated";
+	//int dd = 5; 
+	//size_t sdd = sizeof(dd);
+
+	//void* testa = Testing.malloc(sdd);
+	
+	//int a  = 5; 
+	//void* test1 = Testing.malloc(sizeof(a));
+	
+	//cout << endl; 
+	//cout << testa; 
 	//void *  b = ttt - tt; 
 
 	//int g = static_cast<int>(tt); 
 
+	/*
 	int g = (int)tt; 
 	cout << endl; 
 	cout << g; 
@@ -36,7 +88,7 @@ int main() {
 	cout << endl; 
 	m.print();
 	//out << "Hello Universe";
-
+	*/
 
 
 
