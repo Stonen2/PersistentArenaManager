8ff05ae46f3239de0093b394de708a6390800b0b
Nick Stone <35820605+Stonen2@users.noreply.github.com>
2019-07-30 13:51:12 -0400

    Add files via upload

---
+++ V2.0 Arena Alloc/test22.cpp
@@ -0,0 +1,78 @@
+#pragma once 
+#include <iostream>
+#include "superultra.h"
+#include <chrono>
+#include <iostream>
+#include <fstream>
+#include "implement.h"
+using namespace std;
+
+int main() {
+	auto start = std::chrono::high_resolution_clock::now();
+
+
+
+
+
+
+	void* t = malloc(4);
+	void* tt = malloc(4);
+	void* ttt = malloc(4);
+	void* tttt = malloc(4);
+	void* ttttt = malloc(4);
+	malloc(4);
+	malloc(4);
+	malloc(4);
+	malloc(4);
+	malloc(4);
+
+
+	malloc(8);
+	malloc(8);
+	malloc(8);
+	malloc(8);
+	malloc(8);
+	malloc(8);
+	malloc(8);
+	malloc(8);
+	malloc(8);
+	malloc(8);
+
+
+	//5 uints
+	malloc(8);
+	malloc(8);
+	malloc(8);
+	malloc(8);
+	malloc(8);
+
+
+	//Allocate 20 more ints
+	malloc(4);
+	malloc(4);
+	malloc(4);
+	malloc(4);
+	malloc(4);
+	malloc(4);
+	malloc(4);
+	malloc(4);
+	malloc(4);
+	malloc(4);
+	malloc(4);
+	malloc(4);
+	malloc(4);
+	malloc(4);
+	malloc(4);
+
+	auto finish = std::chrono::high_resolution_clock::now();
+
+	std::chrono::duration<double> elapsed = finish - start;
+
+
+	std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+	//cout << elapsed.count();
+
+
+
+	return 0;
+}
---
+++ V2.0 Arena Alloc/test222cpp.cpp
@@ -0,0 +1,31 @@
+#pragma once
+#include<iostream>
+#include<chrono>
+using namespace std; 
+
+int main() {
+	void* te = malloc(1000000);
+	auto start = std::chrono::high_resolution_clock::now();
+
+
+
+	malloc(64);
+	malloc(64);
+	malloc(64);
+	malloc(64);
+	malloc(64);
+
+
+
+	auto finish = std::chrono::high_resolution_clock::now();
+
+	std::chrono::duration<double> elapsed = finish - start;
+
+
+	std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+	//cout << elapsed.count();
+
+
+
+	return 0;
+}
