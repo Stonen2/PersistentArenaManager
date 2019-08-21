d90bb3089d23ab7733cb1151129e9e13e2e6eef8
Nick Stone <35820605+Stonen2@users.noreply.github.com>
2019-08-20 20:23:13 -0400

    Add files via upload
    Base of the Algorithms is being Created

--- PersisArenaSkeleton/TheAllocator.h
+++ PersisArenaSkeleton/TheAllocator.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <atomic>
 
+#include "Arena.h"
 
 //What should the allocator do? 
 
@@ -56,6 +58,9 @@ using size_type = size_t;
 
 
 
+
+
+
 class TheAllocator {
 private: 
 
@@ -71,6 +76,31 @@ private:
 
 	}
 	size_type pad(size_type t) {
+		if (t < 64) {
+			return 64; 
+		}
+		else if (t < 128) {
+			return 128; 
+		}
+		else if (t < 256) {
+			return 256; 
+		}
+		else if (t < 512) {
+			return 512; 
+		}
+		else if (t < 1024) {
+			return 1024;
+		}
+		else if (t < 2048) {
+			return 2048; 
+		}
+		else if (t < 4096) {
+			return 4096;
+		}
+		else {
+
+			return t * 2; 
+		}
 
 
 	}
@@ -84,11 +114,45 @@ public:
 		start = base;
 		TotalSize = offset;
 		end = &base + offset;
+
+	}
+	void* getmem(size_type s) {
+		size_type findring = pad(s);
+		if (findring == 64) {
+			//ALgorithm for 64
+		}
+		else if (findring == 128) {
+
+		}
+		else if (findring == 256) {
+
+		}
+		else if (findring == 512) {
+
+		}
+		else if (findring == 1024) {
+
+
+		}
+		else if (findring == 2048) {
+
+		}
+		else if (findring == 4096) {
+
+		}
+		else {
+
+
+		}
+
+
 	}
 
 
 
-
+	void destroy() {
+		//No -Op
+	}
 
 
 
